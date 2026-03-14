53d3a6b0b2b244dc00eeb79bc0f2e0735e1e4bf8
Lucas Mauboussin <dragonmemo492@gmail.com>
2026-03-14 15:38:58 +0100

    Update loader.js

--- public/loader.js
+++ public/loader.js
@@ -6,13 +6,8 @@ const LOADER = {
     ],
 	"ComploTYSM FR":
     [
-      "Nouvelle théorie du complot :\nc'est(DETERMINANT)[2|Donne un adjectif qui précède un nom :|][1|Donne un objet (la première boite de texte est pour le déterminant, ex : le/la/les):|DETERMINANT][3|Donne un adjectif qui suit un nom :|]qui[5|Donne un effet (qui ...):|]quand[4|Donne une action (quand ...):|]",
-      "Nouvelle théorie du complot :\nc'est[1|la][2|plus petite][1|chaise][3|bleue]qui[5|tire des rayons laser]quand[4|tu la croques]."
-    ],
-	"Phineas & Ferb FR":
-    [
-      "J'ai trouvé un artefact, c'est(DETERMINANT)[2|Donne un adjectif qui précède un nom :|][1|Donne un objet (la première boite de texte est pour le déterminant, ex : le/la/les):|DETERMINANT][3|Donne un adjectif qui suit un nom :|]qui[5|Donne un effet (qui ...):|]quand[4|Donne une action (quand ...):|]",
-      "J'ai trouvé un artefact, c'est[1|la][2|plus petite][1|chaise][3|bleue]qui[5|tire des rayons laser]quand[4|tu la croques]."
+      "Nouvelle théorie du complot :[1|Donne l'objet concerné par la théorie :|][2|Donne un verbe qui impact l'objet de la théorie :|][3|Donne le complément de l'objet de la première phrase :|]. En effet, si[4|Donne un objet prouvant la théorie :|][5|Donne un verbe (et un complément si nécessaire) prouvant la théorie :|]c'est parce que[4|Donne un objet prouvant la théorie :|][6|Donne une justification montrant que l'objet prouvant la théorie affecte l'objet de la théorie :|][1|Donne l'objet concerné par la théorie :|].",
+      "Nouvelle théorie du complot :[1|la Terre][2|est][3|plate]. En effet, si[4|le soleil][5|disparait]c'est parce que[4|le soleil][6|passe en dessous de][1|la Terre]."
     ],
 	"Artifactory EN":
 	[
